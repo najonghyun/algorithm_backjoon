8d117077e070646faddf515e6b22fee4146d84bd
나종현 <102370959+najonghyun@users.noreply.github.com>
2025-04-09 18:19:01 +0900

    Update 서버 증설 횟수.java

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,14 +1,28 @@
 import java.util.*;
-
+/**
+*
+* 제목 : 프로그래머스_서버증설횟수
+* 설명 : 이 문제는 서버가 증설이 되었을 때 각 서버의 보관을 잘해야 한다. 시간이 지남에 따라 서버의 
+* 증/감 을 해주기 위해서 queue를 사용했다. (가독성을 위해 변수명 중요!)
+* 
+* hour : 현재 시간(끝시간 기준)
+* serverList : 서버 근황 담은 2차원 queue {종료시간, 서버수}
+* serverCount : 현재 증설된 서버 수
+* needs : 이용자에 따른 추가해야할 서버 수
+* extraNeeds : 현재 시간에 증설된 서버 수를 고려하여 더 추가해야 할 서버 수 (needs - serverCount)
+* 
+*
+* author: 나종현
+*/
 class Solution {
     public int solution(int[] players, int m, int k) {
         int answer = 0;
-        Queue<int[]> serverEHour = new ArrayDeque<>();
+        Queue<int[]> serverList = new ArrayDeque<>();
         int serverCount = 0;
         for(int hour=1; hour<=24; hour++) {
-            if(!serverEHour.isEmpty() && hour == serverEHour.peek()[0]){
-                serverCount -= serverEHour.peek()[1];
-                serverEHour.poll();
+            if(!serverList.isEmpty() && hour == serverList.peek()[0]){
+                serverCount -= serverList.peek()[1];
+                serverList.poll();
             }
             
             int needs = players[hour-1] / m;
@@ -18,7 +32,7 @@ class Solution {
                     answer += extraNeeds;
                     serverCount += extraNeeds;
                     
-                    serverEHour.offer(new int[]{hour+k, extraNeeds});
+                    serverList.offer(new int[]{hour+k, extraNeeds});
                 }
             }
         }
