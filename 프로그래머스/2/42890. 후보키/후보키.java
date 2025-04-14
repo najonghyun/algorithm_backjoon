3ccaa9a6dc03b0ff85f692acc08d4348274b74ba
나종현 <102370959+najonghyun@users.noreply.github.com>
2025-04-14 23:21:55 +0900

    Update 후보키.java

--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -1,6 +1,18 @@
 import java.util.*;
-
+/**
+*
+* 제목 : 후보키
+* 설명 : 일단 모든 경우의 수의 속성을 돌면서 유일성과 최소성을 만족하는지 봐야한다. 그래서 부분집합 활용!
+* 1. 유일성 : 뽑은 속성의 값들을 하나의 string으로 합쳐서 같은게 있는지 비교함으로서 유일한지 판별
+* 2. 최소성 : 유일성을 만족하는 것들은 미리 result 배열에 넣어놓고, 유일성에 들어가기전에 이미 그것을 포함한 집합이
+* 존재하는지 확인한다. (이때, 단순 contains()를 사용하면 "0123이 03을 포함한다"에서 예외가 발생하므로 하나씩 봐야함)
+* 3. 그렇게 판별하여 모두 만족하는 select 집합을 result 배열에 넣어주고 size를 출력한다.!
+*
+* author: 나종현
+*
+*/
 class Solution {
+    // 유일성을 만족하는지
     public boolean isUniqueKey(String[][] relation, int row, String select){ 
         ArrayList<String> list = new ArrayList<>();
         for(int i=0; i<row; i++){
@@ -15,6 +27,7 @@ class Solution {
         return true;
     }
     
+    // 최소성을 만족하는지
     public boolean isMinKey(ArrayList<String> result, String select){
         for(String s : result){
             int size = s.length();
