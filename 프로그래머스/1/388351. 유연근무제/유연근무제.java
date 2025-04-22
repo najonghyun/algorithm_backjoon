3dddfd3f630d36e6d4b54de5d125b14012a5c4f3
나종현 <102370959+najonghyun@users.noreply.github.com>
2025-04-23 00:35:27 +0900

    [level 1] Title: 유연근무제, Time: 6.61 ms, Memory: 90.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean isSave(int time, int targetTime) {
+        int endTime = time+10;
+        if(endTime % 100 >= 60){
+            endTime = endTime - 60 + 100;
+        }
+        if(endTime >= targetTime) {
+            return true;
+        }
+        if(endTime >= 2400) {
+            endTime -= 2400;
+            if(endTime >= targetTime) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public int solution(int[] schedules, int[][] timelogs, int startday) {
+        int n = schedules.length;
+        int answer = n;
+        for(int i=0; i<n; i++){
+            int nowDay = startday;
+            for(int j=0; j<7; j++){
+                if(nowDay > 7){
+                    nowDay = 1;
+                }
+                if(nowDay >= 6){
+                    nowDay++;
+                    continue;
+                }
+                if(!isSave(schedules[i], timelogs[i][j])){
+                    System.out.println(timelogs[i][j]);
+                    answer--;
+                    break;
+                }
+                nowDay++;
+            }
+        }
+        return answer;
+    }
+}
