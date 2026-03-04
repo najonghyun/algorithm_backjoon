91117916a5bd20b5ddd54958e941dca9910a9179
나종현 <102370959+najonghyun@users.noreply.github.com>
2026-03-04 22:55:50 +0900

    [Gold IV] Title: 지뢰찾기, Time: 88 ms, Memory: 9368 KB -BaekjoonHub

--- node.js/백준/Gold/9082. 지뢰찾기/지뢰찾기.js
+++ node.js/백준/Gold/9082. 지뢰찾기/지뢰찾기.js
@@ -1,7 +1,12 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split(/\r?\n/);
-
+/**
+ * 설명 : dfs를 사용하면 시간초과가 난다. 그래서 자세히 보면 처음 바로 아래 지뢰의 유무로 2번만 돌리면 
+ * 답을 구할 수 있다. 왜냐하면 그 후에는 그 다음인 i+1에 해당하는 것만 보면 되기 때문이다. 
+ * 그래서 그리디로 o(N) 만에 해결할 수 있다.
+ * + 보는 중간에라도 더 많아진다면 그것은 안되므로 바로 빠져나와야 한다.
+ */
 function getBombCount(n, now, visited) {
     let count = 0;
     const prev = now - 1;
@@ -20,9 +25,6 @@ const solution = () => {
         const N = Number(input[index++]);
         const list = input[index++].split("").map(Number);
         const bombs = input[index++].split("");
-
-        // console.log(list);
-        // console.log(visited);
 
         let result = 0;
         for (let i = 0; i < 2; i++) {
@@ -53,7 +55,6 @@ const solution = () => {
                 }
             }
             if (possible && list[N - 1] === getBombCount(N, N - 1, visited)) {
-                // console.log(count);
                 result = Math.max(result, count);
             }
         }
