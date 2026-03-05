f82385cfe0ea6045f6afd0193d4492323194c60c
나종현 <102370959+najonghyun@users.noreply.github.com>
2026-03-06 01:25:49 +0900

    [Silver III] Title: 계단 오르기, Time: 92 ms, Memory: 9356 KB -BaekjoonHub

---
+++ node.js/백준/Silver/2579. 계단 오르기/계단 오르기.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split(/\r?\n/);
+
+const solution = () => {
+    const N = Number(input[0]);
+    const list = new Array(N);
+    for (let i = 1; i <= N; i++) {
+        list[i - 1] = Number(input[i]);
+    }
+    // console.log(list);
+    const dp = new Array(N).fill(0);
+    if (N > 0) {
+        dp[0] = list[0];
+    }
+    if (N > 1) {
+        dp[1] = dp[0] + list[1];
+    }
+    if (N > 2) {
+        dp[2] = Math.max(list[0] + list[2], list[1] + list[2]);
+    }
+    for (let i = 3; i < N; i++) {
+        dp[i] = Math.max(dp[i - 2] + list[i], dp[i - 3] + list[i - 1] + list[i])
+    }
+    console.log(dp[N - 1]);
+
+
+};
+solution();
