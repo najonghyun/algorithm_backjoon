ce66fa95e74f8fe9a6859e48524f0ed6d8ee9b64
나종현 <102370959+najonghyun@users.noreply.github.com>
2026-03-06 01:26:41 +0900

    [Silver III] Title: 계단 오르기, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

--- node.js/백준/Silver/2579. 계단 오르기/계단 오르기.js
+++ node.js/백준/Silver/2579. 계단 오르기/계단 오르기.js
@@ -1,14 +1,15 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split(/\r?\n/);
-
+/**
+ * 설명 : dfs는 시간초과고 dp를 사용해야 한다. 점화식을 잘 찾아야 한다.
+ */
 const solution = () => {
     const N = Number(input[0]);
     const list = new Array(N);
     for (let i = 1; i <= N; i++) {
         list[i - 1] = Number(input[i]);
     }
-    // console.log(list);
     const dp = new Array(N).fill(0);
     if (N > 0) {
         dp[0] = list[0];
@@ -23,7 +24,5 @@ const solution = () => {
         dp[i] = Math.max(dp[i - 2] + list[i], dp[i - 3] + list[i - 1] + list[i])
     }
     console.log(dp[N - 1]);
-
-
 };
 solution();
