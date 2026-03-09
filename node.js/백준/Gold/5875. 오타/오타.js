5318adcbb72c3cd2653d9916bdf77b8495a5bc0d
나종현 <102370959+najonghyun@users.noreply.github.com>
2026-03-09 23:17:34 +0900

    [Gold II] Title: 오타, Time: 172 ms, Memory: 15792 KB -BaekjoonHub

--- node.js/백준/Gold/5875. 오타/오타.js
+++ node.js/백준/Gold/5875. 오타/오타.js
@@ -3,10 +3,11 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split(/\r?\n/);
 const INF = Number.MAX_VALUE;
 /**
- * 설명 : 괄호 뒤집기 문제는 3개 기억해야 한다.
- * 1. sum == ±2
- * 2. prefix[i-1] >= 0
- * 3. suffixMin[i] ± 2 >= 0
+ * 설명 : 괄호 뒤집기 문제이고 o(N)에 풀어야 시간초과가 나지 않는다.
+ * 무조건 전이든 후든 음수가 되면 괄호 탈락이다. 이 점을 유의해서 3개의 조건으로 푼다.
+ * 1. sum === ±2
+ * 2. leftMin(현재 전까지) >= 0
+ * 3. rightMin(다음 가는 수들) ± 2 >= 0
  */
 const solution = () => {
     const list = input[0].split("");
@@ -32,9 +33,7 @@ const solution = () => {
     for (let i = size - 1; i >= 0; i--) {
         rightMin[i] = Math.min(rightMin[i + 1], prefix[i]);
     }
-    // console.log(prefix);
-    // console.log(leftMin);
-    // console.log(rightMin);
+
     let count = 0;
     if (sum === -2) {
         for (let i = 0; i < size; i++) {
