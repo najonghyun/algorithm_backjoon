b4f8f8bbd9f1855c2af071249713882fa189f43a
나종현 <102370959+najonghyun@users.noreply.github.com>
2026-03-10 23:13:13 +0900

    [Gold II] Title: 우수 마을, Time: 204 ms, Memory: 21112 KB -BaekjoonHub

--- node.js/백준/Gold/1949. 우수 마을/우수 마을.js
+++ node.js/백준/Gold/1949. 우수 마을/우수 마을.js
@@ -1,13 +1,16 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split(/\r?\n/);
-const INF = Number.MAX_VALUE;
+/**
+ * 트리 최대 독립집합
+ * 설명 : 일단 완탐을 해야 할것 같고, 시간초과면 거의 dp이다. 그중에서도 이 문제는 (선택or미선택) 인 2가지 경우이므로 
+ * dp[v][0] : 선택 안했을 때, dp[v][1] : 선택 했을 때 로 나누어서 거꾸로 재귀(dfs)를 사용한다.
+ */
 
 function dfs(adjList, citizen, visited, dp, now) {
     dp[now][1] = citizen[now];
     for (let next of adjList[now]) {
         if (visited[next]) continue;
-
         visited[next] = true;
         dfs(adjList, citizen, visited, dp, next);
         visited[next] = false;
@@ -28,9 +31,6 @@ const solution = () => {
         adjList[s].push(e);
         adjList[e].push(s);
     }
-
-    // console.log(citizen);
-    // console.log(adjList);
 
     const dp = Array.from({ length: N + 1 }, () => new Array(2).fill(0));
     const visited = new Array(N).fill(false);
