be9c6da1b3ec134da006a8035180304d8899c68d
나종현 <102370959+najonghyun@users.noreply.github.com>
2026-03-12 16:30:56 +0900

    [Silver I] Title: 작업, Time: 516 ms, Memory: 84332 KB -BaekjoonHub

--- 백준/Silver/21937. 작업/작업.js
+++ 백준/Silver/21937. 작업/작업.js
@@ -1,55 +1,38 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split(/\r?\n/);
-let count;
-
-function bfs(n, adjList, inCount, x) {
+/**
+ * 설명 : 위상정렬 알고리즘 인줄 알았는데 x값에 해당하는 선행들만 계산해서 그 개수를 구하는 것이므로
+ * 그냥 단순하게 방향 반대로 설정하고 bfs로 x부터 시작해서 갈 수 있는 곳 개수의 합을 출력하면 된다.
+ */
+function bfs(n, adjList, start) {
     const q = [];
-    for (let i = 1; i <= n; i++) {
-        if (inCount[i] === 0) {
-            q.push([i, 0]);
-        }
-    }
-    let head = 0;
+    const visited = new Array(n + 1).fill(false);
+    q.push(start);
+    visited[start] = true;
+    let head = 0,
+        count = 0;
     while (head < q.length) {
-        const [now, count] = q[head++];
-        if (now === x) return count;
-
+        const now = q[head++];
         for (const next of adjList[now] ?? []) {
-            inCount[next]--;
-            if (inCount[next] === 0) {
-                q.push([next, count + 1]);
-            }
+            if (visited[next]) continue;
+            count++;
+            q.push(next);
+            visited[next] = true;
         }
     }
-}
-
-function dfs(adjList, visited, now) {
-    visited[now] = true;
-    for (const next of adjList[now] ?? []) {
-        if (visited[next]) continue;
-        count++;
-        dfs(adjList, visited, next);
-    }
+    return count;
 }
 const solution = () => {
     const [N, M] = input[0].split(" ").map(Number);
-    const inCount = new Array(N + 1).fill(0);
     const adjList = {};
     for (let i = 1; i <= M; i++) {
         const [A, B] = input[i].split(" ").map(Number);
         if (!adjList[B]) adjList[B] = [];
         adjList[B].push(A);
-        inCount[B]++;
     }
     const X = Number(input[M + 1]);
-    // console.log(adjList);
-    // console.log(inCount.slice(1));
-
-    // const result = bfs(N, adjList, inCount, X);
-    count = 0;
-    const visited = new Array(N + 1).fill(false);
-    dfs(adjList, visited, X);
-    console.log(count);
+    const result = bfs(N, adjList, X);
+    console.log(result);
 };
 solution();
