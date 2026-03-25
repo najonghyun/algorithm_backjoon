a80efc92411ac804de1a34f0ccd77a95e0320b60
나종현 <102370959+najonghyun@users.noreply.github.com>
2026-03-25 21:04:14 +0900

    [Gold IV] Title: 동작 그만. 밑장 빼기냐?, Time: 184 ms, Memory: 17396 KB -BaekjoonHub

--- node.js/백준/Gold/20159. 동작 그만． 밑장 빼기냐？/동작 그만． 밑장 빼기냐？.js
+++ node.js/백준/Gold/20159. 동작 그만． 밑장 빼기냐？/동작 그만． 밑장 빼기냐？.js
@@ -1,7 +1,11 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split(/\r?\n/);
-
+/**
+ * 설명 : 계산 해보면 내가 원래 뽑은 순서로 더하다가 밑장을 빼면 상대가 원래 뽑은 순서를 받게 된다.
+ * 그래서 각 원래 뽑을 때의 합을 미리 계산해 놓고 그 사이에 따라 각각의 합을 계산해 더해준 후 그것의 최대값을 구하면 된다.
+ * 이 때 상대 턴에서도 밑장을 뺄 수 있다. 
+ */
 const solution = () => {
     const N = Number(input[0]);
     const list = input[1].split(" ").map(Number);
@@ -15,9 +19,7 @@ const solution = () => {
         sumMe[i + 1] = sumMe[i] + list[odd];
         sumYou[i + 1] = sumYou[i] + list[even];
     }
-    // console.log(sumMe);
-    // console.log(sumYou);
-
+    
     let result = sumMe[count];
     for (let i = 0; i < count; i++) {
         const case1 = (sumMe[i] - sumMe[0]) + (sumYou[count] - sumYou[i]); // 내 차례 밑장
