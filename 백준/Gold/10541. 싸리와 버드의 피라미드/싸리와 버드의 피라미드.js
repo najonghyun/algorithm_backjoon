a1d74809d1bcbb520b7647449b105c7f788606de
나종현 <102370959+najonghyun@users.noreply.github.com>
2026-03-27 16:38:53 +0900

    [Gold II] Title: 싸리와 버드의 피라미드, Time: 2424 ms, Memory: 349928 KB -BaekjoonHub

--- 백준/Gold/10541. 싸리와 버드의 피라미드/싸리와 버드의 피라미드.js
+++ 백준/Gold/10541. 싸리와 버드의 피라미드/싸리와 버드의 피라미드.js
@@ -4,45 +4,38 @@ const input = fs.readFileSync(filePath).toString().trim().split(/\r?\n/);
 
 const solution = () => {
     let index = 0;
-    const N = BigInt(input[index++]); // 사실 안 써도 되지만 맞춰둠
+    const N = Number(input[index++]);
     const word = input[index++].split("");
     const size = word.length;
     const bigSize = BigInt(size);
-
     const wordSum = new Array(size + 1);
     wordSum[0] = {};
     for (let i = 65; i < 91; i++) {
         wordSum[0][String.fromCharCode(i)] = 0;
     }
-
     for (let i = 1; i <= size; i++) {
         wordSum[i] = { ...wordSum[i - 1] };
         wordSum[i][word[i - 1]]++;
     }
+    // console.log(wordSum);
 
     const K = Number(input[index++]);
-    const result = new Array(K);
-
+    const result = new Array(K).fill(0);
     for (let i = 0; i < K; i++) {
         const [a, c] = input[index++].split(" ");
         const num = BigInt(a);
-
         const startPoint = Number(((num * (num - 1n)) / 2n) % bigSize);
         const q = num / bigSize;
-        const r = Number(num % bigSize);
-
         result[i] = BigInt(wordSum[size][c]) * q;
-
+        const r = Number(num % bigSize);
         if (startPoint + r > size) {
-            result[i] += BigInt(wordSum[size][c] - wordSum[startPoint][c]);
+            result[i] += BigInt(wordSum[size][c]) - BigInt(wordSum[startPoint][c]);
             const extra = startPoint + r - size;
             result[i] += BigInt(wordSum[extra][c]);
         } else {
-            result[i] += BigInt(wordSum[startPoint + r][c] - wordSum[startPoint][c]);
+            result[i] += BigInt(wordSum[startPoint + r][c]) - BigInt(wordSum[startPoint][c]);
         }
     }
-
-    console.log(result.map(v => v.toString()).join("\n"));
+    console.log(result.map((v) => v.toString()).join("\n"));
 };
-
 solution();
