34c925ed6995327fda07b7fedad8adc476e3ca15
나종현 <102370959+najonghyun@users.noreply.github.com>
2026-03-31 16:41:20 +0900

    [Gold IV] Title: 함께 블록 쌓기, Time: 164 ms, Memory: 10540 KB -BaekjoonHub

--- 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.js
+++ 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.js
@@ -1,17 +1,18 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split(/\r?\n/);
-
+/**
+ * 설명 : 안뽑는 경우의 수도 더하기 위해 각각의 높이에 0을 기본으로 추가하고 이제 반드시 하나는 뽑아야 하는
+ * 경우의 수를 dp로 풀었다.
+ */
 const solution = () => {
     const [N, M, H] = input[0].split(" ").map(Number);
     const list = new Array(N).fill();
     for (let i = 1; i <= N; i++) {
         list[i - 1] = [0, ...input[i].split(" ").map(Number)];
     }
-    // console.log(list);
 
     const dp = Array.from({ length: N + 1 }, () => new Array(H + 1).fill(0));
-
     dp[0][0] = 1;
     for (let i = 0; i < N; i++) {
         for (const h of list[i]) {
@@ -22,9 +23,7 @@ const solution = () => {
             }
         }
     }
-    // for (let i = 0; i <= N; i++) {
-    //     console.log(dp[i]);
-    // }
+
     console.log(dp[N][H]);
 };
 
