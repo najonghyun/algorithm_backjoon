dc55bec6ba912d12f4d321e43b92146c61e56813
나종현 <102370959+najonghyun@users.noreply.github.com>
2026-04-12 16:51:26 +0900

    [Gold IV] Title: 사전 순 최대 공통 부분 수열, Time: 152 ms, Memory: 9640 KB -BaekjoonHub

--- node.js/백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.js
+++ node.js/백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.js
@@ -1,39 +1,47 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
-
-function compare(arr1, arr2) {
-    const len = Math.min(arr1.length, arr2.length);
-    for (let i = 0; i < len; i++) {
-        if (arr1[i] !== arr2[i]) {
-            return arr1[i] > arr2[i] ? arr1 : arr2;
-        }
-    }
-    return arr1.length > arr2.length ? arr1 : arr2;
-}
-
+/**
+ * 설명 : 분석해보면 이 문제는 같은 공통 수열의 가장 큰 첫 수 이후 나머지 부분에서 또 가장 큰 수 ... 
+ * 이런식으로 접근해 나가면 답을 구할 수 있다. 값이 <= 100이므로 그렇게 크지 않기 때문에 그리디로
+ * 풀 수 있다.
+ */
 const solution = () => {
     const N = Number(input[0]);
     const A = input[1].split(" ").map(Number);
     const M = Number(input[2]);
     const B = input[3].split(" ").map(Number);
 
-    const dp = Array.from({ length: N + 1 }, () => Array.from({ length: M + 1 }, () => []));
-    for (let i = N - 1; i >= 0; i--) {
-        for (let j = M - 1; j >= 0; j--) {
-            let best = compare(dp[i + 1][j], dp[i][j + 1]);
-            if (A[i] === B[j]) {
-                const temp = [A[i], ...dp[i + 1][j + 1]];
-                best = compare(best, temp);
+    let aStart = 0, bStart = 0;
+    const result = [];
+    while (aStart < N && bStart < M) {
+        let found = false;
+        for (let num = 100; num >= 1; num--) {
+            let aIdx = -1, bIdx = -1;
+            for (let i = aStart; i < N; i++) {
+                if (A[i] === num) {
+                    aIdx = i;
+                    break;
+                }
             }
-            dp[i][j] = best;
+            if (aIdx === -1) continue;
+            for (let j = bStart; j < M; j++) {
+                if (B[j] === num) {
+                    bIdx = j;
+                    break;
+                }
+            }
+            if (bIdx === -1) continue;
+            result.push(num);
+            aStart = aIdx + 1;
+            bStart = bIdx + 1;
+            found = true;
+            break;
         }
+        if (!found) break;
     }
-    const size = dp[0][0].length;
-    console.log(size);
-    if (size > 0) {
-        console.log(dp[0][0].join(" "));
-    }
+    console.log(result.length);
+    console.log(result.join(" "));
 };
 
 solution();
